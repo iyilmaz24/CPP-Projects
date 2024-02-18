071c272526315af474715667e9321d616e78c6fb
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-18 18:17:42 -0500

    test implementations in demoDriver.cpp

--- doubly-linked-list/demoDriver.cpp
+++ doubly-linked-list/demoDriver.cpp
@@ -5,6 +5,11 @@
 using namespace std;
 using namespace cop4530;
 
+// Function object types, for use with remove_if()
+class isOdd {
+public:
+  bool operator() (const int& value) { return (value%2)!=0; }
+};
 
 int main() {
 
@@ -24,10 +29,11 @@ int main() {
 
     cout << endl;
     auto l2iter = l2.begin();
-    cout << "l2iter: " << *l2iter << endl;
+    cout << "l2iter start: " << *l2iter << endl;
     l2iter++;
     cout << "l2iter +1: " << *l2iter << endl;
-    ++l2iter;    ++l2iter;    ++l2iter;    ++l2iter;    ++l2iter;    ++l2iter;    ++l2iter;
+    ++l2iter;    ++l2iter;
+    cout << "after ++l2iter" << endl;
     cout << "l2iter +7: " << *l2iter << endl;
     l2iter--;
     cout << "l2iter -1: " << *l2iter << endl;
@@ -56,25 +62,99 @@ int main() {
     l2.pop_front();
     cout << "l2 after pop_front: " << l2 << endl;
 
+    auto l2copy = l2;
+    cout << "l2copy theSize: " << l2copy.size() << endl;
+    cout << "l2copy: " << l2 << endl;
+    l2copy.pop_back();
+    l2copy.pop_front();
+    cout << "l2copy theSize: " << l2copy.size() << endl;
+    cout << "l2copy: " << l2copy << endl;
 
-// this is broken because we need to reverse the prev and next pointers
-// when we flipped the list, we did not flip the pointers so print or << wont work
-    cout << "l2 before: " << l2 << endl;
-    l2.reverse();
-    cout << "Empty: " << l2.empty() << endl;
-    cout << "theSize: " << l2.size() << endl;
-    cout << "l2 reversed: " << l2 << endl;
-    l2.clear();
-    cout << "Empty: " << l2.empty() << endl;
-    cout << "theSize: " << l2.size() << endl;
-    cout << "l2 cleared: " << l2 << endl;
-    l2.print(cout);
+    auto l2end = l2copy.end();
+    cout << "l2end: " << *(l2copy.end()) << endl;
+    l2copy.erase(l2end);
+    cout << "l2copy after erase with end(): " << l2copy << endl;
+    
+    auto l2start = l2copy.begin();
+    l2copy.erase(l2start);
+    cout << "l2copy after erase with begin(): " << l2copy << endl;
+
+    List<float> l3(5, 2.4);
+    cout << "l3 before erase with begin(), end(): " << l3 << endl;
+    cout << "l3 size: " << l3.size() << endl;
+    l3.erase(l3.begin(), l3.end());
+    cout << "l3 after erase with begin(), end(): " << l3 << endl;
+    cout << "l3 size: " << l3.size() << endl;
+
+    l3.insert(l3.begin(), 3.5);
+    cout << "l3 after insert w begin(): " << l3 << endl;
+    cout << "l3 size: " << l3.size() << endl;
+    cout << "l3end: " << *(l3.end()) << endl;
+    l3.insert(l3.end(), 4.5);
+    cout << "l3 after insert w end(): " << l3 << endl;
+    cout << "l3 size: " << l3.size() << endl;
+
+    l3.print(cout);
+    cout << endl << l3 << endl;
+    l3.remove(3.5);
+    cout << l3 << endl;
+
+    cout << "l2copy: " << l2copy << endl;
+    l2copy.remove(7);
+    cout << "l2copy after remove(7): " << l2copy << endl;
+    
+    List<int> l4(4, 8);
+    cout << l4 << endl;
+    l4.push_back(9);
+    l4.push_front(7);
+    cout << l4 << endl;
+    cout << "size: " << l4.size() << endl;
+    l4.remove(8);
+    cout << l4 << endl;
+    l4.remove(7);
+    cout << l4 << endl;
+    l4.remove(9);
+    cout << l4 << endl;
+
+    List<int> listRemove(3, 4);
+    listRemove.remove_if(isOdd());
+    cout << "listRemove: " << listRemove << endl;
+    cout << "listRemove size: " << listRemove.size() << endl;
+    listRemove.push_back(5);
+    listRemove.push_back(5);
+    listRemove.push_back(6);
+    listRemove.push_back(7);
+    listRemove.push_front(3);
+    listRemove.push_front(2);
+    cout << "listRemove: " << listRemove << endl;
+    cout << "listRemove size: " << listRemove.size() << endl;
+    listRemove.remove_if(isOdd());
+    cout << "listRemove: " << listRemove << endl;
+    cout << "listRemove size: " << listRemove.size() << endl;
+
+    auto iterConstructEnd = listRemove.end();
+    iterConstructEnd--;
+    auto iterConstructStart = listRemove.begin();
+    iterConstructStart++;
+    List<int> iterConstruct(iterConstructStart, iterConstructEnd);
+    cout << "iterConstruct: " << iterConstruct << endl;
+    cout << "iterConstruct size: " << iterConstruct.size() << endl;
+ 
+  	List<int> iList {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 90, 80, 70, 60, 50};
+    cout << "iList: " << iList << endl;
+    cout << "iList size: " << iList.size() << endl;
+
+    List<int> iList2 = {100, 90, 80, 70, 60, 50};
+    cout << "iList2: " << iList2 << endl;
+    cout << "iList2 size: " << iList2.size() << endl;
 
     return 0;
 }
 
 
-// valgrind --leak-check=full -v ./doubleLinkL.x
+// valgrind --leak-check=full --track-origins=yes -v ./doubleLinkL.x
 
-// g++6 demoDriver.cpp -o doubleLinkL.x
+// gdb doubleLinkL.x vgcore.
+
 // g++ demoDriver.cpp -o doubleLinkL.x
+// g++ -g demoDriver.cpp -o doubleLinkL.x
