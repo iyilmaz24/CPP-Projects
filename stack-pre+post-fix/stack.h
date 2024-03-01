28c5317f027fb54492ca3d9dc90d3dffa4276bff
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-01 14:02:12 -0500

    add final implementations, container as template arg

--- stack-pre+post-fix/stack.h
+++ stack-pre+post-fix/stack.h
@@ -6,11 +6,11 @@
 
 namespace cop4530 {
 
-    template<typename T>
+    template<typename T, class container=std::vector<T> >
     class Stack {
 
         public:
-            std::vector<T> stack;
+            container stack;
             int s_size;
 
             Stack(); // zero-argument constructor   
@@ -18,7 +18,7 @@ namespace cop4530 {
             Stack (const Stack<T>&); // copy constructor
             Stack(Stack<T> &&); // move constructor
 
-            Stack<T>& operator=(const Stack <T>&); // copy assignment operator=
+            Stack<T>& operator=(const Stack<T>&); // copy assignment operator=
             Stack<T>& operator=(Stack<T> &&); // move assignment operator=
 
             bool empty() const; // true if Stack contains no elements, false otherwise
@@ -40,17 +40,17 @@ namespace cop4530 {
 
 
 };
-        template<typename T>
+        template<typename T, class container>
             std::ostream& operator<<(std::ostream& os, const Stack<T>& a); // invokes the print() method to print the Stack<T> a in the specified ostream    
 
-        template<typename T>
+        template<typename T, class container>
             bool operator==(const Stack<T>&, const Stack<T>&); // return true if compared Stacks have the same elements, in same order, false otherwise  
 
-        template<typename T>
+        template<typename T, class container>
             bool operator!=(const Stack<T>&, const Stack<T>&); // opposite of operator==().
 
-        template<typename T>
-            bool operator<=(const Stack<T>& a, const Stack <T>& b); // return true if every element in Stack a is smaller than or equal to the corresponding element of Stack b, false otherwise
+        template<typename T, class container>
+            bool operator<=(const Stack<T>& a, const Stack<T>& b); // return true if every element in Stack a is smaller than or equal to the corresponding element of Stack b, false otherwise
 
 
 #include "stack.hpp"
--- stack-pre+post-fix/stack.hpp
+++ stack-pre+post-fix/stack.hpp
@@ -1,74 +1,86 @@
 
 
-template<typename T>
-cop4530::Stack<T>:: Stack() {
+template<typename T, class container>
+cop4530::Stack<T, container>:: Stack() {
     this->s_size = 0;
 };
-template<typename T>
-cop4530::Stack<T>:: Stack(const Stack<T>& rhs) {
-    *this = rhs;
+template<typename T, class container>
+cop4530::Stack<T, container>:: Stack(const Stack<T>& rhs) {
+    // std::cout << "copy constructor called" << std::endl;
+    this->s_size = 0;
+    for(int i = 0; i < rhs.s_size; i++) {
+        this->push(rhs.stack[i]);
+    }
 };
-template<typename T>
-cop4530::Stack<T>:: Stack(Stack<T>&& rhs) {
-    *this = rhs;
+template<typename T, class container>
+cop4530::Stack<T, container>:: Stack(Stack<T>&& rhs) {
+    // std::cout << "move constructor called" << std::endl;
+    this->s_size = rhs.s_size;
+    stack = rhs.stack;
 };
-template<typename T>
-Stack<T>& cop4530::Stack<T>:: operator=(const Stack<T>& rhs) {
-    this->stack = rhs.stack;
+template<typename T, class container>
+Stack<T>& cop4530::Stack<T, container>:: operator=(const Stack<T>& rhs) {
+    // std::cout << "copy operator called" << std::endl;
+    this->s_size = 0;
+    for(int i = 0; i < rhs.s_size; i++) {
+        this->push(rhs.stack[i]);
+    }
     return *this;
 };
-template<typename T>
-Stack<T>& cop4530::Stack<T>:: operator=(Stack<T>&& rhs) {
-    this->stack = rhs.stack;
+template<typename T, class container>
+Stack<T>& cop4530::Stack<T, container>:: operator=(Stack<T>&& rhs) {
+    // std::cout << "move operator called" << std::endl;
+    std::swap(s_size, rhs.s_size);
+    std::swap(stack, rhs.stack);
     return *this;
 };
-template<typename T>
-cop4530::Stack<T>:: ~Stack() {
+template<typename T, class container>
+cop4530::Stack<T, container>:: ~Stack() {
     this->clear();
 };
 
 
-template<typename T>
-bool cop4530::Stack<T>:: empty() const {
+template<typename T, class container>
+bool cop4530::Stack<T, container>:: empty() const {
     return this->s_size == 0;
 };
-template<typename T>
-void cop4530::Stack<T>:: clear() {
+template<typename T, class container>
+void cop4530::Stack<T, container>:: clear() {
     this->stack.clear();
     this->s_size = 0;
 };
-template<typename T>
-int cop4530::Stack<T>:: size() const {
-    return this->s_size();
+template<typename T, class container>
+int cop4530::Stack<T, container>:: size() const {
+    return this->s_size;
 };
 
 
-template<typename T>
-void cop4530::Stack<T>:: push(const T& x) {
-    this->stack[this->s_size];
+template<typename T, class container>
+void cop4530::Stack<T, container>:: push(const T& x) {
+    this->stack.push_back(x);
     this->s_size += 1;
 };
-template<typename T>
-void cop4530::Stack<T>:: push(T && x) {
-    this->stack[this->s_size];
+template<typename T, class container>
+void cop4530::Stack<T, container>:: push(T && x) {
+    this->stack.push_back(x);
     this->s_size += 1;
 };
-template<typename T>
-void cop4530::Stack<T>:: pop() {
+template<typename T, class container>
+void cop4530::Stack<T, container>:: pop() {
     this->s_size -= 1;
 };
-template<typename T>
-T& cop4530::Stack<T>:: top() {
+template<typename T, class container>
+T& cop4530::Stack<T, container>:: top() {
     return this->stack[(this->s_size - 1)];;
 };
-template<typename T>
-const T& cop4530::Stack<T>:: top() const {
+template<typename T, class container>
+const T& cop4530::Stack<T, container>:: top() const {
     return this->stack[(this->s_size - 1)];
 };
 
 
-template<typename T>
-void cop4530::Stack<T>:: print(std::ostream &os, char ofc) const {
+template<typename T, class container>
+void cop4530::Stack<T, container>:: print(std::ostream &os, char ofc) const {
     for(int i = 0; i < this->s_size; i++) {
         std::cout << this->stack[i] << ofc;
     };
@@ -76,44 +88,37 @@ void cop4530::Stack<T>:: print(std::ostream &os, char ofc) const {
 
 
 
-template<typename T>
-std::ostream& cop4530::operator<<(std::ostream &os, const cop4530::Stack<T> &a) {
+template<typename T, class container>
+std::ostream& cop4530::operator<<(std::ostream &os, const cop4530::Stack<T, container> &a) {
     a.print(os);
     return os;
 }
-template<typename T>
-bool cop4530::operator==(const cop4530::Stack<T>& a, const cop4530::Stack<T>& b) {
-    if(a.s_size() != b.s_size()) {
+template<typename T, class container>
+bool cop4530::operator==(const cop4530::Stack<T, container>& a, const cop4530::Stack<T, container>& b) {
+    if(a.s_size != b.s_size) {
         return false;
     }
-    while(a.s_size() != 0) {
-        auto x1 = a.top();
-        auto x2 = b.top();
+    for(int i = a.s_size - 1; i >= 0; i--) {
+        auto x1 = a.stack[i];
+        auto x2 = b.stack[i];
         if(x1 != x2) {
             return false;
         }
-        a.pop();
-        b.pop();
     };
     return true;
 }
-template<typename T>
-bool cop4530::operator!=(const cop4530::Stack<T>& a, const cop4530::Stack<T>& b) {
+template<typename T, class container>
+bool cop4530::operator!=(const cop4530::Stack<T, container>& a, const cop4530::Stack<T, container>& b) {
     return (!(a == b));
 }
-template<typename T>
-bool cop4530::operator<=(const cop4530::Stack<T>& a, const cop4530::Stack<T>& b) {
-    if(a.s_size() != b.s_size()) {
-        return false;
-    }
-    while(a.s_size() != 0) {
-        auto x1 = a.top();
-        auto x2 = b.top();
-        if(x1 != x2) {
+template<typename T, class container>
+bool cop4530::operator<=(const cop4530::Stack<T, container>& a, const cop4530::Stack<T, container>& b) {
+    for(int i = a.s_size - 1; i >= 0; i--) {
+        auto x1 = a.stack[i];
+        auto x2 = b.stack[i];
+        if(x1 > x2) {
             return false;
         }
-        a.pop();
-        b.pop();
     };
     return true;
 }
