84e1b922850930263360ab8aa15cbe17050531c7
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-23 19:08:48 -0400

    add comments to h file, method signatures to hpp file

--- binary-exp-tree/bet.h
+++ binary-exp-tree/bet.h
@@ -2,35 +2,39 @@
 #define BE_TREE_H
 
 
+using namespace std;
+
 class BET {
 
-    struct BinaryNode {
+    struct BinaryNode { // public by default
 
-        string element;
-        BinaryNode* left;
-        BinaryNode* right;
+        string element; // the data being stored internally
+        BinaryNode* left; // pointer to left child
+        BinaryNode* right; // pointer to right child
 
         BinaryNode(const string& e = string{}, BinaryNode *l = nullptr, BinaryNode *r = nullptr) 
-            : element{e}, left{l}, right{r} {}
+            : element{e}, left{l}, right{r} {} // initialize BinaryNode data members
     };
         
     public:
-        BET();
-        BET(const string& postfix);
-        BET(const BET&);
-        ~BET();
-        bool buildFromPostfix(const string& postfix);
-        const BET& operator= (const BET&);
-        void printInfixExpression();
-        void printPostfixExpression();
-        size_t size();
-        size_t leaf_nodes();
-        bool empty();
+        BET(); // default zero-parameter constructor
+        BET(const string& postfix); // 1 param constructor -- build tree from the postfix expression 
+        BET(const BET&); // copy constructor -- makes deep copy
+        ~BET(); // destructor -- cleans up all dynamic space
+
+        bool buildFromPostfix(const string& postfix); // build tree from the postfix expression 
+                                                // return true if successful, false otherwise 
+        const BET& operator= (const BET&); // assignment operator = -- makes deep copy
+        void printInfixExpression(); // print infix exp
+        void printPostfixExpression(); // print postfix exp
+        size_t size(); // return number of nodes in tree
+        size_t leaf_nodes(); // return number of leaf nodes in tree
+        bool empty(); // return true if tree is empty, false otherwise
 
     private:
         void printInfixExpression(BinaryNode* n);
-        void makeEmpty(BinaryNode*& t);
-        BinaryNode * clone(BinaryNode* t);
+        void makeEmpty(BinaryNode*& t); // delete all nodes in subtree pointed to by "t"
+        BinaryNode* clone(BinaryNode* t); // clone all nodes in subtree pointed to by "t"
         void printPostfixExpression(BinaryNode* n);
         size_t size(BinaryNode* t);
         size_t leaf_nodes(BinaryNode* t);
--- binary-exp-tree/bet.hpp
+++ binary-exp-tree/bet.hpp
@@ -2,6 +2,57 @@
 
 BET :: BET() {
     // default constructor
-}
+};
+BET :: BET(const string& postfix) {
 
+};
+BET :: BET(const BET&) {
+
+};
+BET :: ~BET() {
+
+};
+bool BET :: buildFromPostfix(const string& postfix) {
+
+};
+const BET& BET :: operator= (const BET&) {
+
+};
+void BET :: printInfixExpression() {
+
+};
+void BET :: printPostfixExpression() {
+
+};
+size_t BET :: size() {
+
+};
+size_t BET :: leaf_nodes() {
+
+};
+bool BET :: empty() {
+
+};
+
+
+// ------------ PRIVATE METHODS ------------ //
+
+void BET :: printInfixExpression(BinaryNode* n) {
+
+};
+void BET :: makeEmpty(BinaryNode*& t) {
+
+};
+BET::BinaryNode* BET :: clone(BinaryNode* t) {
+
+};
+void BET :: printPostfixExpression(BinaryNode* n) {
+
+};
+size_t BET :: size(BinaryNode* t) {
+
+};
+size_t BET :: leaf_nodes(BinaryNode* t) {
+
+};
 
