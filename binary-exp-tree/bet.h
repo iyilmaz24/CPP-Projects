3f0666fa819e38120ddc93124c7cfe871e40bb76
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-23 18:42:57 -0400

    add method declarations in header file

--- binary-exp-tree/bet.h
+++ binary-exp-tree/bet.h
@@ -12,15 +12,28 @@ class BET {
 
         BinaryNode(const string& e = string{}, BinaryNode *l = nullptr, BinaryNode *r = nullptr) 
             : element{e}, left{l}, right{r} {}
-
     };
         
     public:
         BET();
-
+        BET(const string& postfix);
+        BET(const BET&);
+        ~BET();
+        bool buildFromPostfix(const string& postfix);
+        const BET& operator= (const BET&);
+        void printInfixExpression();
+        void printPostfixExpression();
+        size_t size();
+        size_t leaf_nodes();
+        bool empty();
 
     private:
-    
+        void printInfixExpression(BinaryNode* n);
+        void makeEmpty(BinaryNode*& t);
+        BinaryNode * clone(BinaryNode* t);
+        void printPostfixExpression(BinaryNode* n);
+        size_t size(BinaryNode* t);
+        size_t leaf_nodes(BinaryNode* t);
 
 };
 
