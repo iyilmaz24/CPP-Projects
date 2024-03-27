45f77426dd212d68a7af9031962ae823fa5c8cf8
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-27 15:57:02 -0400

    test program in demo.cpp

--- binary-exp-tree/demo.cpp
+++ binary-exp-tree/demo.cpp
@@ -1,23 +1,34 @@
 #include "bet.h"
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() { 
 
     // string postfix;
-    // cout << "Enter the first postfix expression: ";
+    // cout << "Enter the postfix expression: ";
     // getline(cin, postfix);
 
+    vector<string> testInputs = {"4 50 6 + +", "4 50 + 6 +", "4 50 + 6 2 * +",
+    "4 50 6 + + 2 *", "a b + c d e + * *", "", " ", "abc" };
 
     BET myTree = BET();
-    std::string myStr {"10 + 10 + 5"};
-    myTree.buildFromPostfix(myStr);
 
-
-
+    for(auto postF : testInputs) {
+        myTree.buildFromPostfix(postF);
+        cout << "my postF: " << postF << endl;
+        cout << "infix: "; myTree.printInfixExpression();
+        cout << "postfix: "; myTree.printPostfixExpression();
+        cout << "node count (size): " << myTree.size(); cout << endl;
+        cout << "leaf count (leaf_nodes): " << myTree.leaf_nodes(); cout << endl;
+        cout << " -- -- -- -- -- " << endl;
+    }
 
     return 0;
 }
+
+// valgrind --leak-check=full --track-origins=yes -v ./demo.x
+// g++ -g demo.cpp -o demo.x
 
